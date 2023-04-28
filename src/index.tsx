a7e995b40207b6dc9f4896bd0160a6f452119b64
crichardd <crichard11@myges.fr>
2023-04-28 15:37:47 +0200

    ajout des chemins à l'index

--- src/index.tsx
+++ src/index.tsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './css/index.css';
-import App from './App';
+
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import Login from './Login';
+import LandingPage from './LandingPage';
+import App from './App';
+import './css/index.css';
+import User from './User';
+import Message from './Message';
+import Friends from './Friends';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}></Route>
+        <Route path="/connect" element={<Login />}></Route>
+        <Route path='/landingpage' element={<LandingPage/>}></Route>
+        <Route path='/Friends' element={<Friends/>}></Route>
+        <Route path='/Message' element={<Message/>}></Route>
+        <Route path='/User' element={<User/>}></Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
