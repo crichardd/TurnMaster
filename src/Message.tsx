565664c7979914bf6c64d3bd245d425b62f31991
crichardd <crichard11@myges.fr>
2023-04-28 16:59:13 +0200

    mise en place de la structure de la page Message

---
+++ src/Message.tsx
@@ -0,0 +1,16 @@
+import Header from "./header";
+
+function Message() {
+  
+
+    return (
+        <div className="App">
+            <Header connect={true}/>
+            <main className='LandingPage-main bg-dark'>
+            
+            </main>
+        </div>
+    );
+}
+
+export default Message;
