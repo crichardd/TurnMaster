50b0a73b6951ea2e6afe36035d9f05882e47cb79
crichardd <crichard11@myges.fr>
2023-04-28 16:58:28 +0200

    mise en place de la landingpage

---
+++ src/LandingPage.tsx
@@ -0,0 +1,21 @@
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { useState} from 'react';
+import Header from './header';
+import './css/burgerMenu.css';
+
+
+function LandingPage() {
+
+  return (
+    <div className="App">
+      <Header connect={true}/>
+
+      <main className='LandingPage-main bg-dark'>
+     
+      </main>
+    </div>
+  );
+}
+
+export default LandingPage;
