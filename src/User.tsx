1b936d6bed4b217fac27fad418b5c8820e26c89e
crichardd <crichard11@myges.fr>
2023-04-28 17:00:24 +0200

    mise en place de la structure de la page utilisateur

---
+++ src/User.tsx
@@ -0,0 +1,19 @@
+import './css/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from "./header";
+
+function User() {
+  
+
+
+  return (
+    <div className="App">
+      <Header connect={true}/>
+      <main className='LandingPage-main bg-dark'>
+      
+      </main>
+    </div>
+  );
+}
+
+export default User;
