5fb6786a8c2ce0cf369e8b1d9a0ef34d895a9ad7
crichardd <crichard11@myges.fr>
2023-05-07 23:32:39 +0200

    changement de login

--- src/Login.tsx
+++ src/Login.tsx
@@ -7,135 +7,219 @@ import { LoginDTO } from './dto/Login.dto';
 import { InscriptionDTO } from './dto/Add.dto';
 import { InscriptionService } from './services/Inscription.service';
 
-function Login(){
-  const [connect, setConnect] = useState<LoginDTO>();
-  const [status, setStatus] = useState<boolean>(false);
-  const navigate = useNavigate();
+export const showSignin = () => {
+  const prism = document.querySelector(".rec-prism") as HTMLElement;
+  if (prism) {
+      prism.style.transform = "translateZ(-100px)";
+  }
+}
+  
+export const showSignup = () => {
+  const prism = document.querySelector(".rec-prism") as HTMLElement;
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateY( -90deg)";
+  }
+}
 
-  //animation signIn signUp
-  const switchersRef = useRef<NodeListOf<Element>>(null);
+export const showForgotPassword = () => {
+  const prism = document.querySelector(".rec-prism") as HTMLElement;
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateY( -180deg)";
+  }
+}
 
-  useEffect(() => {
-    const switchers = switchersRef.current;
+export const showSubscribe = () => {
+  const prism = document.querySelector(".rec-prism") as HTMLElement;
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateX( -90deg)";
+  }
+}
 
-    if (switchers) {
-      const handleClick = function(this: HTMLElement) {
-        switchers.forEach((item) =>
-          item.parentElement?.classList.remove('is-active')
-        );
-        this.parentElement?.classList.add('is-active');
-      };
+export const showContactUs = () => {
+  const prism = document.querySelector(".rec-prism") as HTMLElement;
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateY( 90deg)";
+  }
+}
 
-      switchers.forEach((item) => {
-        item.addEventListener('click', handleClick);
-      });
+export const showThankYou = () => {
+  const prism = document.querySelector(".rec-prism") as HTMLElement;
+  if (prism) {
+    prism.style.transform = "translateZ(-100px) rotateX( 90deg)";
+  }
+}
 
-      return () => {
-        switchers.forEach((item) => {
-          item.removeEventListener('click', handleClick);
-        });
-      };
+export default function Login() {
+
+    const [connect, setConnect] = useState<LoginDTO>();
+    const [status, setStatus] = useState<boolean>(false);
+    const navigate = useNavigate();
+  
+    async function handlelogin(username: any) {
+      const result = await LoginService.getInstance().username(username);
+      setConnect(result);
+      
+      setStatus(true);
+      navigate("/LandingPage");
     }
 
-  }, []);
-
-  async function handlelogin(username: any) {
-    const result = await LoginService.getInstance().username(username);
-    setConnect(result);
-    
-    setStatus(true);
-    navigate("/LandingPage");
-  }
-
-  const handleSubmit = (event: any) => {
-      event.preventDefault();
+    const handleSubmit = (event: any) => {
+        event.preventDefault();
+        
+        const formData = new FormData(event.target);
+        const username = formData.get("username");
+        const password = formData.get("password");
       
-      const formData = new FormData(event.target);
-      const username = formData.get("username");
-      const password = formData.get("password");
-    
-      handlelogin({ username, password });
-  };
+        handlelogin({ username, password });
+    };
 
-  const [inscription, setInscription] = useState<InscriptionDTO>();
+    const [inscription, setInscription] = useState<InscriptionDTO>();
+  
+    async function handleInscription(inscription: any) {
+      const result = await InscriptionService.getInstance().inscription(
+        inscription
+      );
+      setInscription(result);
+      navigate("/user", { state: { username: inscription.username } });
+    }
 
-  async function handleInscription(inscription: any) {
-    const result = await InscriptionService.getInstance().inscription(
-      inscription
-    );
-    setInscription(result);
-    navigate("/user", { state: { username: inscription.username } });
-  }
+    const handleSubmitInscription = (event: any) => {
+      event.preventDefault();
 
-  const handleSubmitInscription = (event: any) => {
-    event.preventDefault();
-
-    const username = event.target.username.value;
-    const password = event.target.elements.password.value;
-
-    handleInscription({ username, password });
-  };
+      const username = event.target.username.value;
+      const name = event.target.name.value;
+      const firstname = event.target.firstname.value;
+      const password = event.target.elements.password.value;
+  
+      handleInscription({ username, name, firstname, password });
+    };
 
   return (
-    <div className="App">
-      <header className="App-header bg-light">
-        <div className='mint titleMain'><h1>TurnMaster</h1> </div>
+    <div className="Login">
+      <header className="Login-header">
       </header>
-      <main className='LandingPage-main'>
-        <section className="forms-section">
-
-          <div className="forms">
-            <div className="form-wrapper is-active">
-              <button type="button" className="switcher switcher-login">
-                Login
-                <span className="underline"></span>
-              </button>
-              <form className="form form-login" onSubmit={handleSubmit}>
-                <fieldset>
-                  <legend>Please, enter your username and password for login.</legend>
-                  <div className="input-block">
-                    <label htmlFor="login-email">Username</label>
-                    <input id="signup-email" name="username" type="text" required/>
-                  </div>
-                  <div className="input-block">
-                    <label htmlFor="login-password">Mot de passe</label>
-                    <input id="login-password" type="password" name="password" required/>
-                  </div>
-                </fieldset>
-                <button type="submit" className="btn-login">Login</button>
-              </form>
+      <body className='tmp'>
+        <ul className="nav">
+          <li onClick={showSignin}>Connexion</li>
+          <li onClick={showSignup}>Inscription</li>
+          <li onClick={showSubscribe}>News Letter</li>
+          <li onClick={showContactUs}>Contactez-nous</li>
+        </ul>
+        <div className="wrapper">
+          <div className="rec-prism">
+            <div className="face face-top">
+              <div className="content">
+              <h2>News Letter</h2>
+              <small>Entrez votre username pour recevoir les dernières mises à jour !</small>
+              <form onSubmit={(event) => event.preventDefault()}>
+                <div className="field-wrapper">
+                  <input type="text" name="username" placeholder="e-mail"/>
+                  <label>Username</label>
+                </div>
+                <div className="field-wrapper">
+                  <input type="submit" onClick={showThankYou}/>
+                </div>
+                </form>
+              </div>
             </div>
-
-            
-            <div className="form-wrapper">
-              <button type="button" className="switcher switcher-signup">
-                Sign Up
-                <span className="underline"></span> 
-              </button>
-              <form className="form form-signup" onSubmit={handleSubmitInscription}>
-                <fieldset>
-                  <legend>Please, enter your username, password and password confirmation for sign up.</legend>
-                  <div className="input-block">
-                    <label htmlFor="signup-email">Username</label>
-                    <input id="signup-email" name="username" type="text" required/>
+            <div className="face face-front">
+              <div className="content">
+                <h2>Connexion</h2>
+                <form onSubmit={handleSubmit}>
+                  <div className="field-wrapper">
+                    <input type="text" name="username" placeholder="nom d'utilisateur"/>
+                    <label>Username</label>
                   </div>
-                  <div className="input-block">
-                    <label htmlFor="signup-password">Password</label>
-                    <input id="signup-password" name="password" type="password" required/>
+                  <div className="field-wrapper">
+                    <input type="password" name="password" placeholder="mot de passe" autoComplete="new-password"/>
+                    <label>mot de passe</label>
                   </div>
-                  <div className="input-block">
-                    <label htmlFor="signup-password-confirm">Confirm password</label>
-                    <input id="signup-password-confirm" type="password" required/>
+                  <div className="field-wrapper">
+                    <input type="submit"/>
                   </div>
-                </fieldset>
-                <button type="submit" className="btn-signup">Continue</button>
+                  <span className="psw" onClick={showForgotPassword}>Mot de passe oublié ? </span>
+                  <span className="signup" onClick={showSignup}>Pas encore de compte ? Inscrivez-vous</span>
+                </form>
+              </div>
+            </div>
+            <div className="face face-back">
+              <div className="content">
+                <h2>Mot de passe oublié ?</h2>
+                <small>Entrez votre e-mail pour recevoir un lien de réinitialisation de votre mot de passe</small>
+                <form onSubmit={(event) => event.preventDefault()}>
+                  <div className="field-wrapper">
+                    <input type="text" name="username" placeholder="e-mail"/>
+                    <label>Username</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit" onClick={showThankYou}/>
+                  </div>
               </form>
+              </div>
+            </div>
+            <div className="face face-right">
+              <div className="content">
+                <h2>Inscription</h2>
+                <form onSubmit={handleSubmitInscription}>
+                  <div className="field-wrapper">
+                    <input type="text" name="username" placeholder="username"/>
+                    <label>Username</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="text" name="name" placeholder="name"/>
+                    <label>Nom</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="text" name="firstname" placeholder="firstname"/>
+                    <label>Prénom</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="password" name="password" placeholder="password" autoComplete="new-password"/>
+                    <label>Mot de passe</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="password" name="password2" placeholder="password" autoComplete="new-password"/>
+                    <label>Entrer à nouveau votre mot de passe</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit"/>
+                  </div>
+                  <span className="singin" onClick={showSignin}>Déjà utilisateur ? Connexion</span>
+                </form>
+              </div>
+            </div>
+            <div className="face face-left">
+              <div className="content">
+                <h2>Contactez-nous</h2>
+                <form onSubmit={(event) => event.preventDefault()}>
+                  <div className="field-wrapper">
+                    <input type="text" name="name" placeholder="name"/>
+                    <label>Nom</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="text" name="username" placeholder="username"/>
+                    <label>Username</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <textarea placeholder="your message"></textarea>
+                    <label>Votre message</label>
+                  </div>
+                  <div className="field-wrapper">
+                    <input type="submit" onClick={showThankYou}/>
+                  </div>
+                </form>
+              </div>
+            </div>
+            <div className="face face-bottom">
+              <div className="content">
+                <div className="thank-you-msg">
+                  Thank you!
+                </div>
+              </div>
             </div>
           </div>
-        </section>
-      </main>
+        </div>
+      </body>
     </div>
   );
 }
-
-export default Login;
