040d9538a14ef53effe99c847adf779d7ebf41b3
crichardd <crichard11@myges.fr>
2023-05-08 12:10:54 +0200

    retirer commentaire

--- src/header.tsx
+++ src/header.tsx
@@ -27,31 +27,3 @@ function Header({ connect }: { connect: boolean }){
 }
 
 export default Header;
-
-/*
-
-{location.pathname.indexOf("user") !== -1 || connect === true ? (
-                <Navbar bg="light" expand="lg" style={{ height: "80px", minHeight: "80px",  width: "100%" }}>
-                    <Container>
-                        <Navbar.Brand href="" onClick={handleLandingPage}>
-                            <div className='mint titleMain'><h1> TurnMaster </h1> </div>
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                        <Navbar.Collapse id="basic-navbar-nav">
-                            <Nav className="me-auto">
-                                <Nav.Link href="" onClick={handleFriends}>
-                                    Amis
-                                </Nav.Link>
-                                <Nav.Link href="" onClick={handleMessages}>
-                                    Conversation
-                                </Nav.Link>
-                                <Nav.Link href="" onClick={handleProfil}>
-                                    Profil
-                                </Nav.Link>
-                            </Nav>
-                        </Navbar.Collapse>
-                    </Container>
-                </Navbar>
-            ):(
-                <div> </div>
-            )}*/
