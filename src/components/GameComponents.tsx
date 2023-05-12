2a9cbbd02ce5341d898909430a9fe0f4c6661541
crichardd <crichard11@myges.fr>
2023-05-12 21:43:23 +0200

    ajout de la structure des components Game et Library

---
+++ src/components/GameComponents.tsx
@@ -0,0 +1,14 @@
+import { useLocation, useNavigate } from "react-router-dom";
+import '../css/header.css'
+
+  
+function GameComponents(){
+
+
+    return (
+        <div></div>
+    );
+
+}
+
+export default GameComponents();
