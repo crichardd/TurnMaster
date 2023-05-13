a5d034a24cecd0bb6157613697c732d1172d3ef3
crichardd <crichard11@myges.fr>
2023-05-13 09:59:18 +0200

    all

--- src/components/GameComponents.tsx
+++ src/components/GameComponents.tsx
@@ -1,14 +1,26 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
-
-  
 function GameComponents(){
 
 
     return (
-        <div></div>
+        <div>
+            <form id="create-party-form">
+                <label htmlFor="party-name">Nom de la partie:</label>
+                <input type="text" id="party-name" name="partyName" required/>
+
+                <label htmlFor="num-players">Nombre de joueurs:</label>
+                <input type="number" id="num-players" name="numPlayers" min="2" max="8" required/>
+
+                <label htmlFor="rules">Règles:</label>
+                <textarea id="rules" name="rules" required></textarea>
+
+                <button type="submit">Créer une partie</button>
+            </form>
+
+        </div>
     );
 
 }
 
-export default GameComponents();
+export default GameComponents;
