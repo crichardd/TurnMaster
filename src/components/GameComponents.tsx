67fddad8aacce762bc5d603405f1aaf47286436e
crichardd <crichard11@myges.fr>
2023-05-13 13:20:04 +0200

    ajout du handleSubmit au GameComponents

--- src/components/GameComponents.tsx
+++ src/components/GameComponents.tsx
@@ -1,19 +1,35 @@
 import { useLocation, useNavigate } from "react-router-dom";
 
+import { useState, useRef, useEffect} from 'react';
+import { PartyService } from '../services/CreateParty.Service';
+import { PartyDTO } from '../dto/CreateParty.dto';
+
 function GameComponents(){
 
+    const navigate = useNavigate();
+    const location = useLocation();
+    const [partyName, setPartyName] = useState('');
+    const [numPlayers, setNumPlayers] = useState(2);
+
+    const handleSubmit = async (event: any) => {
+        event.preventDefault();
+        const party = new PartyDTO(partyName, location.state.username, numPlayers);
+        try {
+            const response = await PartyService.createParty(party);
+            navigate('/party/' + response.partyId);
+        } catch (error) {
+            console.error(error);
+        }
+    };
 
     return (
         <div>
-            <form id="create-party-form">
+            <form id="create-party-form" onSubmit={handleSubmit}>
                 <label htmlFor="party-name">Nom de la partie:</label>
                 <input type="text" id="party-name" name="partyName" required/>
 
                 <label htmlFor="num-players">Nombre de joueurs:</label>
                 <input type="number" id="num-players" name="numPlayers" min="2" max="8" required/>
-
-                <label htmlFor="rules">Règles:</label>
-                <textarea id="rules" name="rules" required></textarea>
 
                 <button type="submit">Créer une partie</button>
             </form>
